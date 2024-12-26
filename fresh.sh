6116bc99f507c9c18db61947e5c1ce1bdaa1ca89
Elias Athey <athey.elias@gmail.com>
2024-12-25 22:37:20 -0800

    add ending output message

--- fresh.sh
+++ fresh.sh
@@ -43,3 +43,15 @@ source ./.macos
 
 # Install Powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "...Mac setup complete"
+echo ""
+echo "Install manually:"
+echo "1. EVO (audio interface driver)"
+echo "2. VS Code Extensions:"
+echo "GitLens"
+echo "GitHub Copilot" 
+echo ""
+echo "Configure manually:"
+echo "1. ssh key (/bin/sh ssh.sh)"
+echo "2. OBS profiles"
